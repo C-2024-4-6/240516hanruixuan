44806ec41f5edb81ade670cc72b6fd0de16aa2a2
MingHe-tomato <3448590054@qq.com>
2024-12-13 11:37:10 +0800

    Update shiyan4.3.cpp

--- shiyan3,4/shiyan4.3.cpp
+++ shiyan3,4/shiyan4.3.cpp
@@ -3,23 +3,17 @@
 using namespace std;
 
 int main() {
-    vector<bool> lockers(100, false); // 初始化所有存物柜为关闭状态
-
-    // 模拟每个学生的操作
-    for (int i = 1; i <= 100; ++i) {
-        for (int j = i - 1; j < 100; j += i) {
-            lockers[j] = !lockers[j]; // 改变存物柜状态
-        }
-    }
-
-    // 输出状态为开的存物柜编号
+    vector<bool> lockers(100, true); 
     cout << "开着的存物柜编号为：";
-    for (int i = 0; i < 100; ++i) {
-        if (lockers[i]) {
-            cout << i + 1 << " "; // 编号从1开始，所以需要加1
+    for (int i = 2; i <=100; i++) {
+        for (int j = i; j < 100; j = ++j + i) {
+            lockers[j] = !lockers[j];       //改变状态
         }
     }
-    cout << endl;
-
+    for (int i = 1; i <= 100; ++i) {
+        if (lockers[i]) {
+            cout << i << " ";
+        }
+    }
     return 0;
 }
